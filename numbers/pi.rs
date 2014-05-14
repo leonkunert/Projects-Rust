7659db4e41200e3799126dca88661e0edcd94fb1
Leon Kunert <info@leonkunert.de>
2014-05-15 01:46:51 +0200

    Added Pi file and numbers folder

---
+++ numbers/pi.rs
@@ -0,0 +1,21 @@
+use std::f32::to_str_digits;
+use std::num::atan;
+
+fn get_pi(num :uint) ->~str {
+    to_str_digits(16f32*atan(1f32/5f32)-4f32*atan(1f32/239f32), num)
+}
+
+#[test]
+fn get_pi_test() {
+    assert!(get_pi(12) == to_str_digits(3.1415927410125732421875f32, 12), "{} {}", get_pi(12), 3.141592741013);
+}
+
+#[test]
+fn get_pi_test2() {
+    assert!(get_pi(0) == to_str_digits(3.1415927410125732421875f32, 0), "{} {}", get_pi(0), 3);
+}
+
+fn main() {
+    let pi = get_pi(12);
+    println!("{}", pi);
+}
