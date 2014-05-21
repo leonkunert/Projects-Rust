8a73040e930b8819e767b34267b0b912eec1173a
Leon Kunert <leonkunert@me.com>
2014-05-21 22:32:45 +0200

    Let the tests for cost_return pass.

--- numbers/cost_return.rs
+++ numbers/cost_return.rs
@@ -5,12 +5,17 @@ fn calculate_return(cost :f64, given :f64) -> f64 {
 
 #[test]
 fn calculate_return_test(){
-    assert!(calculate_return(13.23f64, 15.00f64) == 1.77f64, "expected {:?} is {:?}", 1.77f64, calculate_return(13.23f64, 15.00f64));
+    assert_eq!(calculate_return(13.23f64, 15.00f64), 15f64-13.23f64);
 }
 
 #[test]
 fn calculate_return_test2(){
-    assert!(calculate_return(132.23f64, 150.00f64) == 17.77f64, "expected {:?} is {:?}", 17.77f64, calculate_return(132.23f64, 150.00f64));
+    assert_eq!(calculate_return(132.23f64, 150.00f64), 150f64-132.23f64);
+}
+
+#[test]
+fn calculate_return_test3(){
+    assert_eq!(calculate_return(1f64, 2f64), 1f64);
 }
 
 fn main() {
